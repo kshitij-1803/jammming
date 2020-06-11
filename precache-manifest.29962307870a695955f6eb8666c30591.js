4de4941fd4d3dca58ecf72fbdebf135aed83b5e8
kshitij-1803 <62874526+kshitij-1803@users.noreply.github.com>
2020-06-11 10:35:29 +0530

    first commit

---
+++ precache-manifest.29962307870a695955f6eb8666c30591.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52233b48e3d59948b494eab210287dac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21f28827f20e22c2d153",
+    "url": "/static/css/main.b21a2e05.chunk.css"
+  },
+  {
+    "revision": "43bb5cec1544d9163fd4",
+    "url": "/static/js/2.074bae49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.074bae49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f28827f20e22c2d153",
+    "url": "/static/js/main.cc24209f.chunk.js"
+  },
+  {
+    "revision": "b4185d4afb8be5c52d7c",
+    "url": "/static/js/runtime-main.ef8d2aa8.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
